3211c5744f5bcc4c8e6e1ed124def694e27b347e
Moncef Ramdane <monceframdane2009@gmail.com>
2022-12-24 14:59:04 +0100

    9

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -4,7 +4,7 @@ const Task = require('../models/Task')
 
 const getTasks = asyncHandler(async (req, res) => {
     try {
-        const tasks = await Task.find()
+        const tasks = await Task.find({ user: req.user.id })
         res.status(200).json(tasks)
     } catch (error) {
         res.status(400).json({ message: error.message })
@@ -15,7 +15,7 @@ const getTasks = asyncHandler(async (req, res) => {
 const setTask = asyncHandler(async (req, res) => {
     try {
         const task = await Task.create({
-            user: req.user,
+            user: req.user.id,
             title: req.body.title,
             description: req.body.description,
             createdAt: Date.now(),
@@ -36,6 +36,15 @@ const updateTask = asyncHandler(async (req, res) => {
         res.status(404).json({ message: 'pas de tache pour cette Id' })
     }
 
+
+    if (!req.user) {
+        res.status(401).json({ message: 'User not found' })
+    }
+
+    if (task.user.toString() !== req.user.id) {
+        res.status(401).json({ message: 'User not authorized' })
+    }
+
     const updateTask = await Task.findByIdAndUpdate(req.params.id, {
         title: req.body.title,
         description: req.body.description,
@@ -48,6 +57,19 @@ const updateTask = asyncHandler(async (req, res) => {
 
 const deletTask = asyncHandler(async (req, res) => {
     const task = await Task.findById(req.params.id)
+
+    if (!task) {
+        res.status(404).json({ message: 'pas de tache pour cette Id' })
+    }
+
+
+    if (!req.user) {
+        res.status(401).json({ message: 'User not found' })
+    }
+
+    if (task.user.toString() !== req.user.id) {
+        res.status(401).json({ message: 'User not authorized' })
+    }
 
     await task.remove()
 
